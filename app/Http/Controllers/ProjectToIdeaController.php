4eafa29462561354c2fef7b73ddf7a7cf362c9c2
denis <emisdb@gmail.com>
2023-03-12 02:01:04 +0300

    add all new

---
+++ app/Http/Controllers/ProjectToIdeaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectToIdea;
+use Illuminate\Http\Request;
+
+class ProjectToIdeaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectToIdea  $projectToIdea
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectToIdea $projectToIdea)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectToIdea  $projectToIdea
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectToIdea $projectToIdea)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProjectToIdea  $projectToIdea
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectToIdea $projectToIdea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectToIdea  $projectToIdea
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectToIdea $projectToIdea)
+    {
+        //
+    }
+}
