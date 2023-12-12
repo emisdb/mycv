2d0a8e3d2901bd8d8f90a8886a45d225605952a2
denis <emisdb@gmail.com>
2023-12-12 16:34:06 +0300

    CV path corrected

--- resources/views/inc/cv-button.blade.php
+++ resources/views/inc/cv-button.blade.php
@@ -1,5 +1,5 @@
 @section('cv-button')
-    <a href="docs/cv_eng_g.pdf" class="w3-button w3-light-grey w3-padding-large w3-margin-top" target="_blank">
+    <a href="{{url('/')}}/docs/cv_eng_g.pdf" class="w3-button w3-light-grey w3-padding-large w3-margin-top" target="_blank">
         <i class="fa fa-download"></i> Download Resume
     </a>
 @endsection
