b8553585b93ac1be04f76d3c4fb920939a8e78ed
denis <emisdb@gmail.com>
2023-12-16 15:06:56 +0300

    new symbols for new DB

--- resources/views/inc/aboutproj.blade.php
+++ resources/views/inc/aboutproj.blade.php
@@ -5,8 +5,7 @@
     </li>
     <li>
         Required project_to_ideas entries:
-        <ul>
-            <li>idea_id [47,46] - team or individual</li>
+               <li>idea_id [47,46] - team or individual</li>
             <li>idea_id [81,82,83] - web, desktop, embed</li>
             <li>idea_id [topic_id(topics[id=1])] - technologies</li>
         </ul>
