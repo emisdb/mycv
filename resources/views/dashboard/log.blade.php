8f526b53ac514d14f614e1158dd3c914cdd56756
denis <emisdb@gmail.com>
2024-03-05 23:14:04 +0300

    log line blade is added with dates at the entry in master

--- resources/views/dashboard/log.blade.php
+++ resources/views/dashboard/log.blade.php
@@ -10,15 +10,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- log/show.blade.php -->
-                    @foreach ($logEntries as $logEntry)
-                        @include('dashboard.log_line', ['entry' => $logEntry])
-
-                        @if(strpos($logEntry, $myIp) !== false)
-                            <p style="background-color: #7a8793">{{ $logEntry }}</p>
-                        @else
-                            <p>{{ $logEntry }}</p>
-                        @endif
+                @php $prevSubstring = ''; @endphp
+                @foreach ($logEntries as $logEntry)
+                        @include('dashboard.log_line',  ['prevSubstring' => $prevSubstring])
+                        @php $prevSubstring = substr($logEntry, 0, 10); @endphp <!-- Update previous substring -->
                     @endforeach
                     {{ $logEntries->links() }}
                 </div>
--- resources/views/dashboard/log_line.blade.php
+++ resources/views/dashboard/log_line.blade.php
@@ -1,3 +1,20 @@
-<hr/>
+<!-- log_entry.blade.php -->
+@php
+    $currentSubstring = substr($logEntry, 0, 10); // Get the first 10 characters
+@endphp
+
+@if ($currentSubstring !== $prevSubstring)
+    <p><strong>{{ $currentSubstring }}</strong></p> <!-- Display the new substring -->
+@endif
+
+@if(strpos($logEntry, $myIp) !== false)
+    <p style="background-color: #7a8793; margin-left:5px;">{{ $logEntry }}</p>
+@else
+    <p style="margin-left:5px;">{{ $logEntry }}</p>
+@endif
+
+
+
+
 
 
